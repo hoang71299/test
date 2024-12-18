2fb8ea8af3eb26c350c2ce19dd144049fd7cca69
Hoang <huynhhoang712003@gmail.com>
2024-12-19 02:30:20 +0700

    hello2

--- src/assets/upload.ts
+++ src/assets/upload.ts
@@ -1,25 +1,25 @@
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
-import {storage} from "../config/Firebase"
+// import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
+// import {storage} from "../config/Firebase"
 
-const urlStorage = `sqc-academy`
+// const urlStorage = `sqc-academy`
 
-export const uploadAvatar = async (file: File): Promise<string> => {
-  try {
-    const storageRef = ref(storage, `${urlStorage}/${file.name}`)
-    console.log("Storage reference created:", storageRef)
+// export const uploadAvatar = async (file: File): Promise<string> => {
+//   try {
+//     const storageRef = ref(storage, `${urlStorage}/${file.name}`)
+//     console.log("Storage reference created:", storageRef)
 
-    const uploadResult = await uploadBytes(storageRef, file)
-    console.log("Upload result:", uploadResult)
+//     const uploadResult = await uploadBytes(storageRef, file)
+//     console.log("Upload result:", uploadResult)
 
-    const downloadURL = await getDownloadURL(storageRef)
-    console.log("Download URL:", downloadURL)
+//     const downloadURL = await getDownloadURL(storageRef)
+//     console.log("Download URL:", downloadURL)
 
-    alert("tải ảnh lên thành công! ")
+//     alert("tải ảnh lên thành công! ")
 
-    return downloadURL
-  } catch (error) {
-    alert("Lỗi khi tải ảnh lên Firebase!")
-    console.error(error)
-    return ""
-  }
-}
+//     return downloadURL
+//   } catch (error) {
+//     alert("Lỗi khi tải ảnh lên Firebase!")
+//     console.error(error)
+//     return ""
+//   }
+// }
